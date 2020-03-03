4bae640b96186db4bde8ada4f6b7a3383af7028e
Jan Hybs <jan.hybs@tul.cz>
2020-03-03 17:33:48 +0100

    Edit pp

--- projects/flow123d/utils/runtest.sh
+++ projects/flow123d/utils/runtest.sh
@@ -2,7 +2,7 @@ make -C flow123d update-build-tree
 
 # execute flow in serial for specific yaml file and mesh size
 # results will be stored in a temp folder with unique ID, so they can be link
-extraPath=<workdir()>/flow123d/src/python
+extraPath=<workdir()>/python-fix/python
 
 export PYTHONPATH=$PYTHONPATH:$extraPath
 flow123d/bin/flow123d \
